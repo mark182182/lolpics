899285b678a917bf001e4af11a0aa606fe05f239
mark182182 <versalife11@hotmail.com>
2018-10-09 09:56:10 +0200

    test(endpoints): check status

--- server-test.js
+++ server-test.js
@@ -4,21 +4,42 @@ const test = require('tape');
 const request = require('supertest');
 const serverModule = require('./server');
 
-test('Check if 1 is equal 1', function (t) {
-  t.equal(1, 1);
+test('/ endpoint should return status 200', (t) => {
+  request(serverModule.app)
+    .get('/')
+    .expect(200)
+    .end(function (err, res) {
+      if (err) {
+        t.error(err);
+      }
+      t.deepEquals(res.status, 200, 'Should return 200');
+    });
   t.end();
 });
 
-test('/api/giphy endpoint', (t) => {
+test('/api/giphy endpoint should return status 200', (t) => {
   request(serverModule.app)
-    .get('/')
+    .get('/api/giphy')
     .expect('Content-Type', /json/)
     .expect(200)
     .end(function (err, res) {
       if (err) {
         t.error(err);
       }
-      t.deepEquals(res.body, { received: res.body.url });
+      t.deepEquals(res.status, 200, 'Should return 200');
+    });
+  t.end();
+});
+
+test('/doesnotexist endpoint should return 404', (t) => {
+  request(serverModule.app)
+    .get('/doesnotexist')
+    .expect(404)
+    .end(function (err, res) {
+      if (err) {
+        t.error(err);
+      }
+      t.deepEquals(res.status, 404, 'Should return 404');
       serverModule.server.close();
     });
   t.end();
